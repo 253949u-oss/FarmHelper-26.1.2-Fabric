89c996f417ad89ba3446bc332ea628cae1590746
253949u-oss <253949u@student.hci.edu.sg>
2026-09-10 16:20:43 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     idea
     java
-    id("fabric-loom") version "1.20"
+    id("net.fabricmc.fabric-loom") version "1.20"
     id("dev.architectury.architectury-pack200") version "0.1.3"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("net.kyori.blossom") version "1.3.2"
@@ -48,28 +48,23 @@ val shadowImpl: Configuration by configurations.creating {
 
 dependencies {
     minecraft("com.mojang:minecraft:$mcVersion")
-    mappings(loom.officialMojangMappings())
     modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
     modImplementation("net.fabricmc.fabric-api:fabric-api:$fabricApiVersion")
-    
-    // Mixin
-    compileOnly("org.spongepowered:mixin:0.12.5")  // Updated for newer Fabric
-    annotationProcessor("org.spongepowered:mixin:0.12.5")
-    
+
     // Lombok
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
-    
+
     // Your dependencies
     shadowImpl("org.java-websocket:Java-WebSocket:1.5.7")
     implementation("net.dv8tion:JDA:5.1.0")
 }
 
-tasks.withType(JavaCompile::class) {
+tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
 
-tasks.withType(Jar::class) {
+tasks.withType<Jar> {
     archiveBaseName.set(modName)
 }
 
@@ -90,8 +85,5 @@ tasks.shadowJar {
     relocate("org.java_websocket", "${baseGroup}.shadow.websocket")
 }
 
-// Make sure the shadow JAR is included in the final mod JAR
-tasks.remapJar {
-    dependsOn(tasks.shadowJar)
-    inputFile.set(tasks.shadowJar.flatMap { it.archiveFile })
-}
+// Use nestJars instead of setting remapJar input for non-remap Loom
+loom.nestJars(tasks.shadowJar)
